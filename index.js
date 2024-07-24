5bd14271f314d4133efd062534f49445025744ed
ivanuria <ivan.uria@gmail.com>
2024-07-24 11:49:23 +0200

    Added app.js

--- index.js
+++ index.js
@@ -1,57 +1,5 @@
 const config = require('./utils/config')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const { jsonResponseHandler } = require('./utils/helper')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', jsonResponseHandler)
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    logger.info('Connected to MongoDB')
-  })
-  .catch(error => {
-    logger.error('Error connecting to MongoDB', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+const app = require('./app')
 
 const PORT = config.PORT
 app.listen(PORT, () => {
