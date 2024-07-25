baecbe505935507d9b54e25bedaf04f081a5c136
ivanuria <ivan.uria@gmail.com>
2024-07-25 12:54:34 +0200

    Separated mongodb connection to another file

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 const config = require('./utils/config')
-const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
-const mongoose = require('mongoose')
 const blogsRoutes = require('./controllers/blogs')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { mongoBdConnect } = require('./utils/mongodb')
 
 const mongoUrl = config.MONGODB_URI
 
-mongoose.set('strictQuery', false)
-
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    logger.info('Connected to MongoDB')
-  })
-  .catch(error => {
-    logger.error('Error connecting to MongoDB', error.message)
-  })
-
+mongoBdConnect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
---
+++ utils/mongodb.js
@@ -0,0 +1,18 @@
+const logger = require('./logger')
+const mongoose = require('mongoose')
+const config = require('./config')
+
+const mongoBdConnect = async (mongoUrl) => {
+  mongoose.set('strictQuery', false)
+
+  try {
+    await mongoose.connect(mongoUrl)
+    logger.info('Connected to MongoDB', mongoUrl)
+  } catch(exception) {
+    logger.error('Error connecting to MongoDB', error.message)
+  }
+}
+
+module.exports = {
+  mongoBdConnect
+}
