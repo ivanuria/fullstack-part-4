9c940786004441fc7e112c381394e0a4820152cf
ivanuria <ivan.uria@gmail.com>
2024-07-25 13:32:54 +0200

    Set test boilerplate

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,15 @@
+const config = require('./config')
+
 const info = (...params) => {
-  console.log(...params)
+  if (config.NODE_ENV !== config.NODE_ENVS.TEST) {
+    console.log(...params)
+  }
 }
 
 const error = (...params) => {
-  console.error(...params)
+  if (config.NODE_ENV !== config.NODE_ENVS.TEST) {
+    console.error(...params)
+  }
 }
 
 module.exports = {
