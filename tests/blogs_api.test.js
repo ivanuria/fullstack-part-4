c34b8e666c8dc19aa183b47e6653f5a9d0dcb95f
ivanuria <ivan.uria@gmail.com>
2024-07-25 17:01:26 +0200

    Ex 4.9

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -4,6 +4,7 @@ const { app, mongod } = require('../app')
 const supertest = require('supertest')
 const { mongoDBDisconnect } = require('../utils/mongodb')
 const helper = require('./blogs_list_helper')
+const lodash = require('lodash')
 
 const api = supertest(app)
 
@@ -17,9 +18,24 @@ describe('blogs list api', async () => {
     await helper.saveBlogs(helper.initialBlogs)
   })
 
+  test('get /api/blogs returns json object', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
   test('get /api/blogs returns the correct amount of blogs', async () => {
     const response = await api.get('/api/blogs')
 
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('get /api/blogs returns the cids fo objects under >>id<< instead of >>_id<<', async () => {
+    const response = await api.get('/api/blogs')
+    console.log(response.body[0])
+    assert.strictEqual('__v' in response.body[0], false)
+    assert.strictEqual('_id' in response.body[0], false)
+    assert('id' in response.body[0])
   })
 })
